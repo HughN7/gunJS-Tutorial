bf098da3b530490442df95866d65314f64f94f56
Hieu Vo <hughvo39@gmail.com>
2023-06-26 19:53:39 -0700

    more examples

--- src/lib/Example2.svelte
+++ src/lib/Example2.svelte
@@ -2,11 +2,11 @@
   import Gun from "gun";
   import { onMount } from "svelte";
 
-  const db_id: string = "t1";
+  const db_id: string = "t4";
   const db = Gun().get(db_id);
 
   //DB Key
-  const state: string = "state"
+  const state: string = "state";
 
   //Local cache storage
   type AppState = {
@@ -14,43 +14,75 @@
     on: boolean | null;
   };
 
-  let store: AppState; 
-  $: store = {val: null, on: null}
+  let store: AppState;
+  $: store = { val: null, on: null };
 
   //Call it once on load
-  onMount(()=>{
-    db.map().once(function(data: AppState){
-      if(data){
-        store = data
+  onMount(() => {
+    db.map().once(function (data: AppState, key: string) {
+      console.log(data);
+      console.log(key);
+      if (data) {
+        store = data;
       }
-    })
-  })
+    });
+  });
 
   //Create a listener
-  db.map().on(function(data: AppState){
-    if(data){
-      store = data
+  db.map().on(function (data: AppState) {
+    if (data) {
+      store = data;
     }
-  })
+  });
 
-  //Crud functions
-  function create(){
-    db.get(state).put({val: -1, on: false})
+  //Crud
+  function create() {
+    console.log("first run?");
+    db.get(state).put({ val: -1, on: false });
   }
 
   function updateON(value: boolean) {
     db.get(state).get("on").put(value);
   }
 
+  function valPlus() {
+    if (store.val != null) {
+      db.get(state)
+        .get("val")
+        .put(store.val + 1);
+    }
+  }
 
+  function valMinus() {
+    if (store.val != null) {
+      db.get(state)
+        .get("val")
+        .put(store.val - 1);
+    }
+  }
 </script>
 
-
+<div>
+  <button on:click={()=> create()}>Create</button>
+  <h1>{store.on == (undefined || null) ? "Not loaded" : store.on}</h1>
+  <br />
+  <h1>{store.val == (undefined || null) ? "Not loaded" : store.val}</h1>
+  <button
+    on:click={() => {
+      updateON(!store.on);
+    }}>Change</button
+  >
+</div>
 
 <div>
-  <button on:click={()=>{create()}}>Create</button>
-  <h1>{store.on == (undefined || null) ? "Not loaded" : store.on}</h1>
-  <br>
-  <h1>{store.val == (undefined || null) ? "Not loaded" : store.val}</h1>
-  <button on:click={()=>{updateON(!store.on)}}>Change</button>
+  <button
+    on:click={() => {
+      valPlus();
+    }}>+1</button
+  >
+  <button
+    on:click={() => {
+      valMinus();
+    }}>-1</button
+  >
 </div>
